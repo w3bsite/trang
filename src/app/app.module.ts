2328c6572585a479a02d2d4ef965a6b2e0166d20
w3bsite <samfisher217201@gmail.com>
2021-10-23 17:26:41 +0330

    added home and page component Empty For now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
